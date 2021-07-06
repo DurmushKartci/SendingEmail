f9d0559a2547fb971190ded90bac02cfb4662394
DurmushKartci <durmusikartci@gmail.com>
2021-07-06 19:50:24 +0300

    Test sonuçlarını gönderen fonksiyon tamamlandı , sure() fonksiyonu yapılacak

--- sketch.js
+++ sketch.js
@@ -1,10 +1,29 @@
 function sendMail() {
+    let sorusayısı = Math.floor(Math.random() * 50);
+    let dogrusayısı = Math.floor(Math.random() * sorusayısı);
+    let yanlıssayısı = Math.floor(Math.random() * sorusayısı - dogrusayısı);
+    console.log(yanlıssayısı)
+    let from_list = ["Durmuş Kartcı", "Muhammed Fatih Kartcı", "Nihan", "Emine Serra"]
+    let from = from_list[Math.floor(Math.random() * from_list.length)]
+
+    let messageFormat = "Merhaba Emin Kartcı,\n " + from + "'nın bugünkü test sonuçları ." +
+        "Soru Sayısı : " + sorusayısı + "\nDoğru Sayısı : " + dogrusayısı + "\Yanlış Sayısı : " + yanlıssayısı + "\nBoş Sayısı : " + (sorusayısı - dogrusayısı - yanlıssayısı) + "\nDoğruluk Oranı : " + ((dogrusayısı * 100 / sorusayısı) / (sorusayısı * 100 / sorusayısı)) + "\nÇözdüğü süre :" + " 2 saat" // + saat + ":" + dakika + ":" + saniye
+
     let tempParams = {
-        from: document.getElementById("from").value,
+        to_name: "Emin Kartcı",
+        from_name: from,
+        total_question: sorusayısı,
+        correct_question: dogrusayısı,
+        wrong_question: yanlıssayısı,
+        time: sure(),
+        space_question: (sorusayısı - dogrusayısı - yanlıssayısı),
+        score: ((dogrusayısı * 100 / sorusayısı) * 100 / (sorusayısı * 100 / sorusayısı)).toFixed(0) + "/100",
         to: document.getElementById("to").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
     }
     console.log(tempParams)
-    emailjs.send("service_880b6o7", "template_0kcpfca", tempParams).then(function(res) { console.log("succes : " + res.status) })
+    emailjs.send("service_880b6o7", "template_cbjq7pg", tempParams).then(function(res) { console.log("succes : " + res.status) })
+}
+
+function sure() {
+    return "00:52:08";
 }
